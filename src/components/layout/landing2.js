efafde198018233c49936793b9292e3a88e3c1fd
Jeff Taylor <jeff.taylor@slalom.com>
2021-04-17 14:57:13 -0400

    added slider for home page

--- src/components/layout/landing2.js
+++ src/components/layout/landing2.js
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import {Link, Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
+import BackgroundSlider from 'react-background-slider'
+import image1 from '../../image-assets/sky-shot.jpg'
+import image2 from '../../image-assets/clay-court.jpg'
+import image3 from '../../image-assets/blue-hard-court.jpg'
+
+
 
 const Landing2 = ({isAuthenticated}) => {
 
-  // if(isAuthenticated) {
-  //   return <Redirect to='/dashboard'/>
-  // }
+  if(isAuthenticated) {
+    return <Redirect to='/dashboard'/>
+  }
 
   return (
-   <div>
-     Landing page placeholder
-    <div>
-      <Link to='/register'>Register</Link>
-    </div>
-    <div>
-      <Link to='/login'>Login</Link>
-    </div>
+   <Fragment>
+     <BackgroundSlider
+      images={[image2, image1, image3]}
+      duration={6}
+      transition={3}
+      />
+     
+    </Fragment>
   
      
-     
-   </div>
   )
 
 }
